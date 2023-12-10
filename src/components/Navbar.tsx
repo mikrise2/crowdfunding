bd6abce6a5e0c5950b24053f4bdcb3f134b7c9c0
mikrise2 <mikrise2@mail.ru>
2023-12-10 16:04:53 +0100

    themes body colors and product page grids

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -146,7 +146,7 @@ function DefaultItem(props: React.PropsWithChildren) {
 
 const Menu = () => {
   return (
-    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
       <BlueItem>Home</BlueItem>
       <DefaultItem>About</DefaultItem>
       <DefaultItem>Services</DefaultItem>
@@ -158,7 +158,7 @@ const Menu = () => {
 
 const Navbar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 max-w-full">
+    <nav className="bg-gray-500 dark:bg-gray-900 max-w-full">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <ThemeSwitcher />
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
